dff27627018c1885dd9b86d97a33a2759712f4f7
huaeraishang <1070351512@qq.com>
2022-07-02 19:21:23 +0800

    添加第257题

---
+++ js/257.二叉树的所有路径.js
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=257 lang=javascript
+ *
+ * [257] 二叉树的所有路径
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string[]}
+ */
+var binaryTreePaths = function(root) {
+    let paths = []
+    const getPaths = (node, path) => {
+        if (node) {
+            path += node.val.toString()
+            if (!node.left && !node.right) {
+                paths.push(path)
+            } else {
+                path += '->'
+                getPaths(node.left, path)
+                getPaths(node.right, path)
+            }
+        }
+    }
+    getPaths(root, '')
+    return paths
+}
+
+// @lc code=end
+
