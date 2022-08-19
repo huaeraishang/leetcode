08e40bb12c15a3e272723df33ac3448b51e20a71
huaeraishang <1070351512@qq.com>
2022-08-19 22:07:57 +0800

    添加 第27 28 58题

---
+++ js/27.移除元素.js
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=27 lang=javascript
+ *
+ * [27] 移除元素
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let len = nums.length
+    while(nums.indexOf(val) !== -1) {
+        nums.splice(nums.indexOf(val), 1)
+    }
+    return nums.length
+};
+// @lc code=end
+
