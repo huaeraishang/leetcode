c9c272e5c47f7d4d9613d24b560def66d9b68134
huaeraishang <1070351512@qq.com>
2023-08-02 21:01:08 +0800

    ✅完成933题：最近的请求次数

---
+++ js/933.最近的请求次数.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=933 lang=javascript
+ *
+ * [933] 最近的请求次数
+ */
+
+// @lc code=start
+
+var RecentCounter = function() {
+  this.requests = []
+};
+
+/** 
+ * @param {number} t
+ * @return {number}
+ */
+RecentCounter.prototype.ping = function(t) {
+  this.requests.push(t)
+  while(this.requests[0] < t - 3000) {
+    this.requests.shift()
+  }
+  return this.requests.length
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * var obj = new RecentCounter()
+ * var param_1 = obj.ping(t)
+ */
+// @lc code=end
+
